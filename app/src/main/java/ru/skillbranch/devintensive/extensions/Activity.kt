d7990129aaa84fdc35f58a5be469524e3df4bba9
arilkaev <xaysdiablo@gmail.com>
2019-12-28 19:24:21 +0500

    isKeyboardOpen() change realization

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -1,23 +1,23 @@
 package ru.skillbranch.devintensive.extensions
 
 import android.app.Activity
+import android.content.Context
 import android.graphics.Rect
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 fun Activity.hideKeyboard() {
-    val view = currentFocus
-    if (view != null) {
-        val imm = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
-    }
+    val content = findViewById<View>(android.R.id.content)
+    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(content.windowToken, 0)
 }
 
 fun Activity.isKeyboardOpen(): Boolean {
     val rect = Rect()
     window.decorView.getWindowVisibleDisplayFrame(rect)
     val screenHeight = window.decorView.rootView.height
-    val difference = screenHeight - (rect.bottom - rect.top)
-    return difference > 200
+    val keyboardHeight = screenHeight - rect.bottom
+    return keyboardHeight > screenHeight * 0.15
 }
 
 fun Activity.isKeyboardClosed(): Boolean {
